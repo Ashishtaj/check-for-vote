212ca83d20180874ab4fa07f6a2fecb8ddb0b705
Ashish mehta <86901123+Ashishtaj@users.noreply.github.com>
2021-11-23 19:35:33 +0530

    Add files via upload

---
+++ check for vote.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int age;
+	cout<<"Enter your age : ";
+	cin>>age;
+	
+	if(age>=18){
+		cout<<"You are able to do vote!!";
+	}else{
+		cout<<"You are not able to do vote!!";
+	}
+return 0;
+}
